21d4e8f25b1f6d3cab21050f951a495c2406f25a
daexs <daelenia.susanto@mail.utoronto.ca>
2022-11-09 23:28:17 -0500

    made User class

--- src/main/java/entities/User.java
+++ src/main/java/entities/User.java
@@ -1,8 +1,24 @@
 package entities;
 
 public abstract class User {
-    private String username;
+
+    private String user;
     private String password;
+
+    public User(String username, String password){
+        this.user = username;
+        this.password = password;
+    }
+
+    public abstract void loginUser();
+
+    public abstract void logoutUser();
+
+
+    public String getUser(){
+        return this.user;
+    }
+
 
 
 }
