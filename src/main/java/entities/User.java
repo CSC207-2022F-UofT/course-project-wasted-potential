01badab1c27d4c0061217ae19c11c1f6dd79aae4
daexs <daelenia.susanto@mail.utoronto.ca>
2022-11-10 22:25:00 -0500

    edited User Class

--- src/main/java/entities/User.java
+++ src/main/java/entities/User.java
@@ -2,12 +2,14 @@ package entities;
 
 public abstract class User {
 
-    private String user;
+    private String username;
     private String password;
+    private User userType;
 
-    public User(String username, String password){
-        this.user = username;
+    public User(String username, String password, User userType){
+        this.username = username;
         this.password = password;
+        this.userType = userType;
     }
 
     public abstract void userLogin();
@@ -15,8 +17,12 @@ public abstract class User {
     public abstract void userLogout();
 
 
-    public String getUser(){
-        return this.user;
+    public String getUsername(){
+        return this.username;
+    }
+
+    public User getUserType(){
+        return this.userType;
     }
 
 
