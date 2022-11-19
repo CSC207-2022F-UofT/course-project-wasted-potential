ff0d234d5e521371356d686723a7b69cda920025
Eshwara Chock <eshchock1@gmail.com>
2022-11-19 18:36:01 -0500

    added complete entities for user, player, designer, and userfactory

--- src/main/java/entities/Designer.java
+++ src/main/java/entities/Designer.java
@@ -1,5 +1,12 @@
 package entities;
 
 public class Designer extends User{
+    private String username;
+    private String password;
+    private String creationTime;
+
+    public Designer(String username, String password, String creationTime){
+        super(username, password, "Designer", creationTime);
+    }
 
 }
