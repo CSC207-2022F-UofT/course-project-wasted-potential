f298c1eddeb7d7138e9166351d8efe869641a13a
daexs <daelenia.susanto@mail.utoronto.ca>
2022-11-20 23:30:22 -0500

    Changes

--- src/main/java/UserLogin/UserLoginController.java
+++ src/main/java/UserLogin/UserLoginController.java
@@ -1,4 +1,16 @@
 package UserLogin;
 
+
 public class UserLoginController {
+    final ULoginInputBoundary userInput;
+
+
+    public UserLoginController(ULoginInputBoundary userInput) {
+        this.userInput = userInput;
+    }
+
+    public UserLoginResponseModel loginUser(String username, String password){
+        UserLoginRequestModel requestModel = new UserLoginRequestModel(username, password);
+        return userInput.logUserIn(requestModel);
+    }
 }
