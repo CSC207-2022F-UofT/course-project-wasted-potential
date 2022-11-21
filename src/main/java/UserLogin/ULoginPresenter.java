861a068bfad7eada80a7e0b2210ee9e39b6ffe84
daexs <113073348+daexs@users.noreply.github.com>
2022-11-21 17:56:52 -0500

    Delete ULoginPresenter.java

--- src/main/java/UserLogin/ULoginPresenter.java
+++
@@ -1,7 +0,0 @@
-package UserLogin;
-
-public interface ULoginPresenter {
-    public UserLoginResponseModel successView(UserLoginResponseModel userResponseModel);
-
-    public UserLoginResponseModel failView(String errorMessage);
-}
