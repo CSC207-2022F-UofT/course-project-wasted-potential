99c6dede8722305c8c3bf6efc33213f1f2da71b0
daexs <daelenia.susanto@mail.utoronto.ca>
2022-12-07 23:07:15 -0500

    Added javadoc to the runtime classes

--- src/main/java/screens/UserCreationError.java
+++ src/main/java/screens/UserCreationError.java
@@ -1,6 +1,14 @@
 package screens;
 
+/**
+ * A subclass of the RuntimeException class when there is an error creating the user.
+ */
 public class UserCreationError extends RuntimeException{
+    /**
+     * Instantiates a new user creation error.
+     *
+     * @param error the error message
+     */
     public UserCreationError(String error) {
         super(error);
     }
